c57c64adaa47c1828cc488a5865cd3dc9744d923
Muhammad Imran <123890319+imranbinmanzoor@users.noreply.github.com>
2025-06-13 16:43:09 +0500

    update

--- snaps.js
+++ snaps.js
@@ -1,7 +1,19 @@
-// From script.js - Mobile nav toggle
-const btnNavEl = document.querySelector(".btn-mobile-nav");
-const headerEl = document.querySelector(".header");
+// From script.js: IntersectionObserver for sticky nav
+const sectionHeroEl = document.querySelector(".section-hero");
 
-btnNavEl.addEventListener("click", function () {
-  headerEl.classList.toggle("nav-open"); /* Toggles class on header */
-});
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky"); /* Adds class to body for sticky styles */
+    } else {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null, // Observe the viewport
+    threshold: 0, // Trigger when 0% of target is visible
+    rootMargin: "-80px", // Trigger 80px before target leaves viewport (accounting for header height)
+  }
+);
+obs.observe(sectionHeroEl);
